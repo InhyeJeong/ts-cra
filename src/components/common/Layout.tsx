1836c42e5c882a96dc8875d7535211f40848eb93
InhyeJeong <inhye.cheong@deliveryhero.co.kr>
2021-05-01 16:20:23 +0900

    DEV feat: Add Home component

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: gray;
+`;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Layout } from '../../components/common/Layout';
+
+const Home: FC = () => {
+  return <Layout>HOME</Layout>;
+};
+
+export default Home;
